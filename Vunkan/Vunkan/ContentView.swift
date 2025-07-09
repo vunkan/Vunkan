c3510a105dcb64978e7fb3a66490b680b51bee82
KikuchiTomo <tomo_kikuchi@icloud.com>
2025-07-09 19:40:11 +0900

    feat: 最初のプロジェクト構成を追加

---
+++ Vunkan/Vunkan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vunkan
+//
+//  Created by Tomo Kikuchi on 2025/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
