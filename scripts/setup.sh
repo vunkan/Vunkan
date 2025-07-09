56f7917ed5e810f1e3e7d9217c7809d37e2959a8
KikuchiTomo <tomo_kikuchi@icloud.com>
2025-07-09 20:39:00 +0900

    feat: xcodegenで生成できるように変更

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,4 +5,12 @@ cd $(dirname $0)/../
 PROJECT_ROOT=`pwd`
 export SCRIPTS=$PROJECT_ROOT/scripts
 
+# Load environment variables
+source $PROJECT_ROOT/env
+
 npm install
+
+# Generate Xcode project using XcodeGen
+envsubst < $PROJECT_ROOT/project.yml > $PROJECT_ROOT/project.generated.yml
+swift run --package-path $SCRIPTS/external/_xcodegen xcodegen generate --spec $PROJECT_ROOT/project.generated.yml
+rm $PROJECT_ROOT/project.generated.yml
